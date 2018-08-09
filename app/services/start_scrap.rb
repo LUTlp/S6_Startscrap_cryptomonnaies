783399b6993099c71a5b8de90cc9889c6eef33eb
LUTlp <lutlp@outlook.com>
2018-08-09 14:54:59 +0200

    test app

--- app/services/start_scrap.rb
+++ app/services/start_scrap.rb
@@ -3,13 +3,30 @@ require 'open-uri'
 require 'nokogiri'
 
 class StartScrap
+attr_accessor :nomcrypto, :prixcrypto
+
   def initialize
     @page = Nokogiri::HTML(open("https://coinmarketcap.com"))
+
   end
 
   def perform
+    Cryptom.destroy_all
+    @page = Nokogiri::HTML(open("https://coinmarketcap.com"))
+    @array_name = [] #Création d'un array "@array_name" vide
+    @array_value = [] #Création d'un array "@array_value" vide
+    page.css('a.currency-name-container').each do |node|
+      @array_name << node.text #Ajoute dans l'array chaque devise
+      end
+    page.css('a.price').each do |node|
+      @array_value << node.text #Ajoute dans l'array chaque devise
+      end
   end
 
+
   def save
+    perform
+    puts Hash[@array_name.zip(@array_value)] #Fait un Hash des deux arrays : "@array_name" et "@array_value"
   end
+
 end
